012bc751ebb6fcbc9db411c44e53ba40c7fedcef
Alissonsz <38736666+Alissonsz@users.noreply.github.com>
2019-04-15 10:13:56 -0300

    começa a implementar parallax mapping

--- main.cpp
+++ main.cpp
@@ -31,9 +31,10 @@ float deltatime = 0.0f;
 float lastframe = 0.0f;
 bool running = true;
 glm::vec3 lightPos(-10.0f, 15.0f, 15.0f);
-Camera camera(glm::vec3(0.0, 5.0f, 6.0f));
+Camera camera(glm::vec3(0.0, 20.0f, 30.0f));
 bool wireframe = false;
 
+void renderQuad();
 bool Init();
 
 void InputProcess(SDL_Event event);
@@ -209,9 +210,10 @@ int main(int argc, char* args[]){
 		ourShader.setVec3("viewPos", camera.Position); 
 
 		//glDrawArrays(GL_POINTS, 0, vert.size());
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		/*glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-		ClearOpenGLErrors();
+		ClearOpenGLErrors();*/
+		renderQuad();
 		our2Shader.use();
 
 		glBindVertexArray(VAO2);
@@ -334,14 +336,116 @@ void InputProcess(SDL_Event event){
 	if(keyboardSnapshot[SDL_SCANCODE_S]) camera.ProcessMouseMovement(0, -10.0f, deltatime);
 	if(keyboardSnapshot[SDL_SCANCODE_D]) camera.ProcessMouseMovement(10.0f, 0, deltatime);
 	if(keyboardSnapshot[SDL_SCANCODE_A]) camera.ProcessMouseMovement(-10.0f, 0, deltatime);
-	if(keyboardSnapshot[SDL_SCANCODE_L]) lightPos.x += 0.2;
-	if(keyboardSnapshot[SDL_SCANCODE_J]) lightPos.x	-= 0.2;
-	if(keyboardSnapshot[SDL_SCANCODE_I]) lightPos.y	+= 0.2;
-	if(keyboardSnapshot[SDL_SCANCODE_K]) lightPos.y	-= 0.2;
+	if(keyboardSnapshot[SDL_SCANCODE_L]) lightPos.x += 1.0f * deltatime;
+	if(keyboardSnapshot[SDL_SCANCODE_J]) lightPos.x	-= 1.0f * deltatime;
+	if(keyboardSnapshot[SDL_SCANCODE_I]) lightPos.y	+= 1.0f * deltatime;
+	if(keyboardSnapshot[SDL_SCANCODE_K]) lightPos.y	-= 1.0f * deltatime;
 	if(keyboardSnapshot[SDL_SCANCODE_M]) glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	if(keyboardSnapshot[SDL_SCANCODE_N]) glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	if(keyboardSnapshot[SDL_SCANCODE_P]){
+		ourShader.use();
+		ourShader.setInt("parFlag", 1);
+	}
+	if(keyboardSnapshot[SDL_SCANCODE_Q]){
+		ourShader.use();
+		ourShader.setInt("parFlag", 0);
+	}	 
 	
 
+}
+
+unsigned int quadVAO = 0;
+unsigned int quadVBO;
+void renderQuad()
+{
+    if (quadVAO == 0)
+    {
+        // positions
+        glm::vec3 pos1(-14.8f,  0.0f, 14.8f);
+        glm::vec3 pos2(-14.8f, 0.0f, -14.8f);
+        glm::vec3 pos3( 14.8f, 0.0f, -14.8f);
+        glm::vec3 pos4( 14.8f,  0.0f, 14.8f);
+        // texture coordinates
+        glm::vec2 uv1(0.0f, 1.0f);
+        glm::vec2 uv2(0.0f, 0.0f);
+        glm::vec2 uv3(1.0f, 0.0f);
+        glm::vec2 uv4(1.0f, 1.0f);
+        // normal vector
+        glm::vec3 nm(0.0f, 1.0f, 0.0f);
+
+        // calculate tangent/bitangent vectors of both triangles
+        glm::vec3 tangent1, bitangent1;
+        glm::vec3 tangent2, bitangent2;
+        // triangle 1
+        // ----------
+        glm::vec3 edge1 = pos2 - pos1;
+        glm::vec3 edge2 = pos3 - pos1;
+        glm::vec2 deltaUV1 = uv2 - uv1;
+        glm::vec2 deltaUV2 = uv3 - uv1;
+
+        float f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+        tangent1.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+        tangent1.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+        tangent1.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+        tangent1 = glm::normalize(tangent1);
+
+        bitangent1.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+        bitangent1.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+        bitangent1.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+        bitangent1 = glm::normalize(bitangent1);
+
+        // triangle 2
+        // ----------
+        edge1 = pos3 - pos1;
+        edge2 = pos4 - pos1;
+        deltaUV1 = uv3 - uv1;
+        deltaUV2 = uv4 - uv1;
+
+        f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+        tangent2.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+        tangent2.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+        tangent2.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+        tangent2 = glm::normalize(tangent2);
+
+
+        bitangent2.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+        bitangent2.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+        bitangent2.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+        bitangent2 = glm::normalize(bitangent2);
+
+
+        float quadVertices[] = {
+            // positions            // normal         // texcoords  // tangent                          // bitangent
+            pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+            pos2.x, pos2.y, pos2.z, nm.x, nm.y, nm.z, uv2.x, uv2.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+            pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+
+            pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+            pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+            pos4.x, pos4.y, pos4.z, nm.x, nm.y, nm.z, uv4.x, uv4.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z
+        };
+        // configure plane VAO
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(8 * sizeof(float)));
+        glEnableVertexAttribArray(4);
+        glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(11 * sizeof(float)));
+    }
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
 }
 
 unsigned int createTerrain(const unsigned char* heightMap, int width){
--- shader.fs
+++ shader.fs
@@ -4,53 +4,104 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 uniform sampler2D normalTexture;
 
-uniform vec3 lightColor;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
-//uniform vec3 normalMap[1050625];
+uniform int parFlag = 0;
 
-in vec2 texCord;
+
+/*in vec2 texCord;
 in vec3 Normal;
-in vec3 FragPos;
+in vec3 FragPos;*/
 
-int MapInRange(float x, float in_min, float in_max, float out_min, float out_max)
-{
+in VS_OUT {
+    vec3 FragPos;
+    vec2 TexCoords;
+    vec3 TangentLightPos;
+    vec3 TangentViewPos;
+    vec3 TangentFragPos;
+} fs_in;
+
+vec2 parallaxMapping(vec2 texCoords, vec3 viewDir){ 
+    // number of depth layers
+    const float numLayers = 60;
+    // calculate the size of each layer
+    float layerDepth = 1.0 / numLayers;
+    // depth of current layer
+    float currentLayerDepth = 0.0;
+    // the amount to shift the texture coordinates per layer (from vector P)
+    vec2 P = viewDir.xy * 0.3; 
+    vec2 deltaTexCoords = P / numLayers;
+
+    // get initial values
+    vec2  currentTexCoords     = texCoords;
+    float currentDepthMapValue = 1 - texture(texture1, currentTexCoords).r;
+    
+    while(currentLayerDepth < currentDepthMapValue)
+    {
+        // shift texture coordinates along direction of P
+        currentTexCoords -= deltaTexCoords;
+        // get depthmap value at current texture coordinates
+        currentDepthMapValue = 1 - texture(texture1, currentTexCoords).r;  
+        // get depth of next layer
+        currentLayerDepth += layerDepth;  
+    }
+
+    vec2 prevTexCoords = currentTexCoords + deltaTexCoords;
+
+    // get depth after and before collision for linear interpolation
+    float afterDepth  = currentDepthMapValue - currentLayerDepth;
+    float beforeDepth = (1 - texture(texture1, prevTexCoords).r) - currentLayerDepth + layerDepth;
+    
+    // interpolation of texture coordinates
+    float weight = afterDepth / (afterDepth - beforeDepth);
+    vec2 finalTexCoords = prevTexCoords * weight + currentTexCoords * (1.0 - weight);
+
+
+    return finalTexCoords;  
+    
+ 
+} 
+
+int MapInRange(float x, float in_min, float in_max, float out_min, float out_max){
     if(x < in_min) x = in_min;
 	if(x > in_max) x = in_max;
 	return int(((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min));
 }
 
 void main() {
-    //calcula a altura certa do fragmento para calculo da iluminacao
-    /*vec3 iUp = vec3(FragPos.x, 0, FragPos.z + 0.1986);
-    vec3 iRight = vec3(FragPos.x + 0.1986, 0, FragPos.z); 
-    vec2 heightMapCord = vec2(MapInRange(FragPos.x, -14.8f, 14.8f, 0, 1), MapInRange(FragPos.z, -14.8f, 14.8f, 0, 1));
-    vec4 newHeight = texture(texture1, heightMapCord);
-    vec3 newCoord = vec3(FragPos.x, newHeight.r, FragPos.z);
-    iUp.y = texture(texture1, vec2(heightMapCord.x, heightMapCord.y + 0.0044)).r ;
-    iRight.y = texture(texture1, vec2(heightMapCord.x + 0.0044, heightMapCord.y)).r ;*/
+    vec3 newPos;
+    vec3 newNormal;
+    //vec2 texCoords = texCord;
+    vec2 texCoords = fs_in.TexCoords;
+    //vec3 viewDir = normalize(viewPos-FragPos);
+    vec3 viewDir = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
 
-    //vec3 newNormal = cross((iUp - newCoord), (iRight - newCoord));
-    vec3 newPos = vec3(FragPos.x, texture(texture1, texCord).r*5.865, FragPos.z);
-    vec3 newNormal = texture(normalTexture, vec2(texCord.x, 0 + (1 - texCord.y))).rgb;
+    if(parFlag == 1)
+        texCoords = parallaxMapping(fs_in.TexCoords, viewDir);
+    if(texCoords.x > 1.0 || texCoords.y > 1.0 || texCoords.x < 0.0 || texCoords.y < 0.0)
+       discard;
+
+    //newPos = vec3(FragPos.x, texture(texture1, texCoords).r*5.865, FragPos.z);
+    newNormal = texture(normalTexture, vec2(texCoords.x, 0 + (1 - texCoords.y))).rgb;
     newNormal = normalize(newNormal * 2.0 - 1.0);
     
-    //vec3 norm = normalize(Normal);
     vec3 norm = newNormal;
-    vec3 lightDir = normalize(lightPos - newPos);
+    vec3 lightColor = texture(texture2, texCoords).rgb;
+    //vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(fs_in.TangentLightPos - fs_in.TangentFragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
 
     float specularStrength = 0.5;
-    vec3 viewDir = normalize(viewPos-newPos);
+    
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = specularStrength * spec * lightColor;  
  
 
-    float ambientStrength = 0.0;
+    float ambientStrength = 0.05;
     vec3 ambient = ambientStrength * lightColor;
 
-    vec4 result = vec4(ambient + diffuse + specular, 1.0) * texture(texture2, texCord);
+    vec4 result = vec4(ambient + diffuse + specular, 1.0);
     FragColor = result;
 }
